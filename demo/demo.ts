0a2c22f390e51ae1aa57b0e7a1f3bbdbf8972461
Yurii Brusentsov <yuriybrusentsov@gmail.com>
2023-07-20 23:52:25 +0300

    chore: update demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,12 +1,23 @@
-/* eslint-disable no-console */
+import { createInterface as createReadlineInterface } from 'node:readline';
+
+import chalk from 'chalk';
+
 import { openExplorer } from '../src/index';
 
-const path = 'C:\\Windows\\System32';
+const rl = createReadlineInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-openExplorer(path)
-  .then(() => {
-    console.log('success');
-  })
-  .catch((error) => {
-    console.error(`Error: ${error}`);
-  });
+rl.question('Path (leave blank to use default value): ', (path) => {
+  openExplorer(path || undefined)
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log(chalk.green('Success'));
+    })
+    .catch((error) => {
+      console.error(chalk.red(`Error: ${error}`));
+    });
+
+  rl.close();
+});
